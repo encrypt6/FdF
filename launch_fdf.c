7cdb5aaa9796a72d5073710bd12af8145becee73
Eliza Sikira <elsikira@paul-f4Ar1s4.clusters.42paris.fr>
2024-07-17 17:17:03 +0200

    getting height done. now getting the width but do it with a ft split

--- launch_fdf.c
+++ launch_fdf.c
@@ -6,7 +6,7 @@
 /*   By: elsikira <elsikira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 12:25:45 by elsikira          #+#    #+#             */
-/*   Updated: 2024/07/16 18:37:02 by elsikira         ###   ########.fr       */
+/*   Updated: 2024/07/17 17:15:35 by elsikira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,37 +25,106 @@ void	free_split(char **split)
 	free(split);
 }
 
-char	*read_map(char *map)
-{
-	int		fd;
-	char	*abs;
 
-	fd = open(map, O_RDONLY);
+int	get_width(char *map_file)
+{
+	t_map	map;
+	char	*abs;
+	int		fd;
+
+	map.width = 0;
+	fd = open (map_file, O_RDONLY);
 	abs = get_next_line(fd);
-	close(fd);
-	return(abs);
+	while (*abs)
+	{
+		abs++;
+		map.width++;
+	}
+	return (map.width);
 }
 
-void	launch_fdf(char *map)
+
+int	get_height(char *map_file)
+{
+	t_map	map;
+	int		fd;
+	char	*abs;
+	map.height = 0;
+	fd = open(map_file, O_RDONLY);
+	while (1)
+	{
+		abs = get_next_line(fd);
+		if (abs == NULL)
+			break ;
+		map.height++;
+		free (abs);
+		abs = NULL;
+	}	
+	close(fd);
+	return(map.height);
+}
+
+void launch_fdf(char *map_file)
 {
 	//t_data	data;
+    t_map map;
+
+    //map.height = 0;
+    //map.width = 0;
+    map.height = get_height(map_file);
+	dprintf(STDOUT_FILENO, "height is : %d\n", map.height);
+  	map.width = get_width(map_file);
+	dprintf(STDOUT_FILENO, "width is : %d\n", map.width);
+    //lines_count = ft_split(abs, '\n');
+    //if (!lines_count)
+       // exit(EXIT_FAILURE);
+    /*while (lines_count[map.height])
+    {
+        map.height++;
+    }*/
+    /*if (map.height > 0)
+    {
+        splitted_abs = ft_split(lines_count[0], ' ');
+        while (splitted_abs[map.width])
+            map.width++;
+    }*/
+	//dprintf(STDOUT_FILENO, "%d\n", map.height);
+  //  free_split(lines_count);
+	/*data.mlx_ptr = mlx_init();
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My FdF");
+	generate_points(data.mlx_ptr, data.win_ptr, splitted_abs, map.width, map.height);
+	mlx_key_hook(data.win_ptr, key_hook, NULL);
+	mlx_loop(data.mlx_ptr);*/
+}
+
+/*void	launch_fdf(char *map_file)
+{
+	t_map	map;
 	char	*abs;
 	char	**splitted_abs;
 
-	abs = read_map(map);
+	map.height = 0;
+	map.width = 0;
+	abs = read_map(map_file);
 	if (!abs)
 		exit(EXIT_FAILURE);
 	splitted_abs = ft_split(abs, ' ');
-	free(abs);
+	//free(abs);
 	if (!splitted_abs)
 		exit(EXIT_FAILURE);
-	dprintf(STDOUT_FILENO, "%s\n", splitted_abs[1]);
-	free_split(splitted_abs);
-	//generate_points();
 	//generate_lines();
-	/*data.mlx_ptr = mlx_init();
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My FdF");
-	mlx_key_hook(data.win_ptr, key_hook, NULL);
-	mlx_loop(data.mlx_ptr);
-	free(points);*/
-}
+	while (abs[map.height])
+	{
+		if (map.width == 0)
+		{
+			while (splitted_abs[map.width])
+				map.width++;
+		}
+	//	free_split(splitted_abs);
+		map.height++;
+	}
+//	free(abs);
+	dprintf(STDOUT_FILENO, "%d\n", map.height);
+
+		//free(points);
+}*/
