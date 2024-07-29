00dae2d2e3c971702e2f194b3c246783b035bb63
parallels <elsikira@student.42.fr>
2024-07-30 00:43:46 +0200

    doing the norm, to do asap : center image, iso view

--- draw.c
+++ draw.c
@@ -14,99 +14,112 @@
 
 float fpart(float x)
 {
-    return x - floorf(x);
+	return(x - floorf(x));
 }
 
 float rfpart(float x)
 {
-    return 1.0 - fpart(x);
+	return(1.0 - fpart(x));
 }
 
 void ft_swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void plot(t_data data, int x, int y, float brightness)
 {
-    int color = (int)(brightness * 255) << 16 | (int)(brightness * 255) << 8 | (int)(brightness * 255);
-    mlx_pixel_put(data.mlx_ptr, data.win_ptr, x, y, color);
+	int color;
+	color = (int)(brightness * 255) << 16 | (int)(brightness * 255) << 8 | (int)(brightness * 255);
+	mlx_pixel_put(data.mlx_ptr, data.win_ptr, x, y, color);
 }
 
 void ft_xiaolin_wu(t_point p0, t_point p1, t_data data)
 {
-    int steep = abs(p1.y - p0.y) > abs(p1.x - p0.x);
-    if (steep)
-    {
-        ft_swap(&p0.x, &p0.y);
-        ft_swap(&p1.x, &p1.y);
-    }
-    if (p0.x > p1.x)
-    {
-        ft_swap(&p0.x, &p1.x);
-        ft_swap(&p0.y, &p1.y);
-    }
+	int	steep;
+	float	dx;
+	float	dy;
+	float	gradient;
+	float	xend;
+	float	yend;
+	float	xgap;
+	int	xpxl1;
+	int	ypxl1;
+	float	intery;
+	int	xpxl2;
+	int	ypxl2;
+	int	x;
 
-    float dx = (float)p1.x - (float)p0.x;
-    float dy = (float)p1.y - (float)p0.y;
-    float gradient = dy / dx;
 
-    float xend = roundf(p0.x);
-    float yend = p0.y + gradient * (xend - p0.x);
-    float xgap = rfpart(p0.x + 0.5);
-    int xpxl1 = xend;
-    int ypxl1 = (int)yend;
-
-    if (steep)
-    {
-        plot(data, ypxl1, xpxl1, rfpart(yend) * xgap);
-        plot(data, ypxl1 + 1, xpxl1, fpart(yend) * xgap);
-    }
-    else
-    {
-        plot(data, xpxl1, ypxl1, rfpart(yend) * xgap);
-        plot(data, xpxl1, ypxl1 + 1, fpart(yend) * xgap);
-    }
-
-    float intery = yend + gradient;
-
-    xend = roundf(p1.x);
-    yend = p1.y + gradient * (xend - p1.x);
-    xgap = fpart(p1.x + 0.5);
-    int xpxl2 = xend;
-    int ypxl2 = (int)yend;
-
-    if (steep)
-    {
-        plot(data, ypxl2, xpxl2, rfpart(yend) * xgap);
-        plot(data, ypxl2 + 1, xpxl2, fpart(yend) * xgap);
-    }
-    else
-    {
-        plot(data, xpxl2, ypxl2, rfpart(yend) * xgap);
-        plot(data, xpxl2, ypxl2 + 1, fpart(yend) * xgap);
-    }
-
-    if (steep)
-    {
-        for (int x = xpxl1 + 1; x < xpxl2; x++)
-        {
-            plot(data, (int)intery, x, rfpart(intery));
-            plot(data, (int)intery + 1, x, fpart(intery));
-            intery += gradient;
-        }
-    }
-    else
-    {
-        for (int x = xpxl1 + 1; x < xpxl2; x++)
-        {
-            plot(data, x, (int)intery, rfpart(intery));
-            plot(data, x, (int)intery + 1, fpart(intery));
-            intery += gradient;
-        }
-    }
+	steep = abs(p1.y - p0.y) > abs(p1.x - p0.x);
+	if (steep)
+	{
+		ft_swap(&p0.x, &p0.y);
+		ft_swap(&p1.x, &p1.y);
+	}
+	if (p0.x > p1.x)
+	{
+		ft_swap(&p0.x, &p1.x);
+		ft_swap(&p0.y, &p1.y);
+	}
+	dx = (float)p1.x - (float)p0.x;
+	dy = (float)p1.y - (float)p0.y;
+	gradient = dy / dx;
+	xend = roundf(p0.x);
+	yend = p0.y + gradient * (xend - p0.x);
+	xgap = rfpart(p0.x + 0.5);
+	xpxl1 = xend;
+	ypxl1 = (int)yend;
+	if(steep)
+	{
+		plot(data, ypxl1, xpxl1, rfpart(yend) * xgap);
+		plot(data, ypxl1 + 1, xpxl1, fpart(yend) * xgap);
+	}
+	else
+	{
+		plot(data, xpxl1, ypxl1, rfpart(yend) * xgap);
+		plot(data, xpxl1, ypxl1 + 1, fpart(yend) * xgap);
+	}
+	intery = yend + gradient;
+	xend = roundf(p1.x);
+	yend = p1.y + gradient * (xend - p1.x);
+	xgap = fpart(p1.x + 0.5);
+	xpxl2 = xend;
+	ypxl2 = (int)yend;
+	if(steep)
+	{
+		plot(data, ypxl2, xpxl2, rfpart(yend) * xgap);
+		plot(data, ypxl2 + 1, xpxl2, fpart(yend) * xgap);
+	}
+	else
+	{
+		plot(data, xpxl2, ypxl2, rfpart(yend) * xgap);
+		plot(data, xpxl2, ypxl2 + 1, fpart(yend) * xgap);
+	}
+	x = xpxl1 + 1;
+	if(steep)
+	{
+		while(x < xpxl2)
+		{
+			plot(data, (int)intery, x, rfpart(intery));
+			plot(data, (int)intery + 1, x, fpart(intery));
+			intery += gradient;
+			x++;
+		}
+	}
+	else
+	{
+		while(x < xpxl2)
+		{
+			plot(data, x, (int)intery, rfpart(intery));
+			plot(data, x, (int)intery + 1, fpart(intery));
+			intery += gradient;
+			x++;
+		}
+	}
 }
 
 t_point	*line_to_points(char *line, int width, int row)
